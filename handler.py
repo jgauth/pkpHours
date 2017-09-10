17a5d7b50da1bd36fcfe891f802bcb1f1aeba22f
John Gauthier <john.gauthier5@gmail.com>
2017-09-09 19:34:30 -0700

    Created add_approve function to add hour submission to TABLE_APPROVE. Removed test 'hello' function.

--- handler.py
+++ handler.py
@@ -1,13 +1,29 @@
 import json
+import boto3
+import os
+import uuid
+from urllib.parse import parse_qs
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+def add_approve(event, context):
+    data = parse_qs(event['body'])
+
+    table = dynamodb.Table(os.environ['TABLE_APPROVE'])
+
+    item = {
+        'id': str(uuid.uuid1()),
+        'uoId': data['uoId'][0],
+        'numHours': data['numHours'][0],
+        'date': data['date'][0],
+        'proctor': data['proctor'][0]
     }
+
+    table.put_item(Item=item)
+
     response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
+    "statusCode": 200,
+    "body": json.dumps(item)
     }
 
     return response
